072bbc9c83e81c3986d9eb19e560c0b38eeab30f
Rajeshwar Reddy <rajeshwarreddy.kadari@gmail.com>
2019-06-23 16:00:46 +0530

    parsingHeaders

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const url = require("url");
 
 let server = http.createServer((req, res) => {
   let parsedUrl = url.parse(req.url, true);
-  // console.log("parsedUrl", parsedUrl);
+  console.log("parsedUrl", parsedUrl);
   let path = parsedUrl.pathname;
   // console.log("path", path);
   let trimmedPath = path.replace(/^\/+|\/+$/g, "");
   // console.log("trimmedPath", trimmedPath);
+  console.log(parsedUrl.query);
+  let queryStringObject = parsedUrl.query;
 
   let method = req.method.toLowerCase();
 
+  let headers = req.headers;
+  console.log("headers", headers);
   res.end("hello world\n");
-  console.log(
-    `request received on " ${trimmedPath} " path whth this method ${method}`
-  );
+  // console.log(`
+  //   request received on " ${trimmedPath} " path
+  //    with this method "${method}"
+  //   and with this query string parameters "${queryStringObject}"
+  // `);
 });
 // start the server, with the port 3000
 server.listen(3000, () => {
